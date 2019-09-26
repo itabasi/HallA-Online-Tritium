c6ca13a64291e82be5d646ae641565ef1082afe8
itabasi <itabashi@dhcp2592>
2019-09-27 00:17:48 +0900

    make new file mmass_calc.cc

---
+++ replay/ana/calc/mass_calc.cc
@@ -0,0 +1,28 @@
+
+
+void mass_calc(){
+
+  double MN,Ma,Bel;
+  int Z,A,L;
+  A=12; // # of Proton
+  Z=27; // total neucleus
+  L=1;  // # of Lambda
+
+  const double Me=0.510998928e6;           // electron      mass (eV/c2)
+  const double Mp = 0.938272046e9;         // proton        mass (eV/c2)
+  const double Mn = 0.939565379e9;         // neutron       mass (eV/c2)
+  const double ML = 1.115683e9;            // Lambda        mass (eV/c2)
+
+  Ma=A*Mp + L*ML + (Z-A-L)*Mn;
+
+  Bel=14.4381*pow(Z,2.39) + 1.55468*1.0e-6*pow(Z,5.35); //eV
+
+  MN=Ma-Z*Me+Bel;
+
+  
+
+  
+  cout<<"mass "<<MN*1.e-9<<" [GeV]"<<endl;
+  
+
+}
